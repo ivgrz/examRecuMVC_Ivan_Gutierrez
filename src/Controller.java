8fa2603a80c117554cf262235949adbd2d200676
Ivan <igutierrezcontreras@danielcastelao.org>
2025-06-18 13:55:00 +0200

    Controller_comentado

--- src/Controller.java
+++ src/Controller.java
@@ -1,5 +1,11 @@
 public class Controller {
-
+    /**
+     *
+     * @param numero1
+     * @param numero2
+     * @param resultado
+     * @return
+     */
     public static Datos obtenerDatosC(int numero1, int numero2, int resultado) {
         Datos datos = new Datos(numero1, numero2, resultado);
         Model model = new Model();
@@ -8,15 +14,47 @@ public class Controller {
 
     }
 
+    /**
+     * Funcion para sumar valores
+     * @param numero1
+     * @param numero2
+     * @param resultado
+     * @return
+     */
+
     public static int SumarC(int numero1, int numero2, int resultado){
         return Model.SumarM(numero1, numero2, resultado);
     }
+
+    /**
+     * Funcion para restar valores
+     * @param numero1
+     * @param numero2
+     * @param resultado
+     * @return
+     */
     public static  int RestarC(int numero1, int numero2, int resultado){
         return Model.RestarM(numero1, numero2, resultado);
     }
+
+    /**
+     * Funcion para multiplicar valores
+     * @param numero1
+     * @param numero2
+     * @param resultado
+     * @return
+     */
     public static int MultiplicarC(int numero1, int numero2, int resultado){
         return Model.MultiplicarM(numero1, numero2, resultado);
     }
+
+    /**
+     * Funcion para dividir valores
+     * @param numero1
+     * @param numero2
+     * @param resultado
+     * @return
+     */
     public static boolean DivisionC(int numero1, int numero2, int resultado){
         return Model.DivisionM(numero1, numero2, resultado);
     }
