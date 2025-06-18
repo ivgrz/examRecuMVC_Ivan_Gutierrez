b5ec947b53f4e5ce9caed6171219ba5579c210ff
Ivan <igutierrezcontreras@danielcastelao.org>
2025-06-18 13:32:42 +0200

    Recopilacion_Datos_MVC

--- src/Datos.java
+++ src/Datos.java
@@ -1,2 +1,22 @@
 public class Datos {
+    private int numero1;
+    private int numero2;
+    private int resultado;
+
+
+    public Datos(int numero1, int numero2,int resultado) {
+        this.numero1 = numero1;
+        this.numero2 = numero2;
+        this.resultado = resultado;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Datos{" +
+                "numero1=" + numero1 +
+                ", numero2=" + numero2 +
+                ",resultado=" + resultado +
+                '}';
+    }
 }
