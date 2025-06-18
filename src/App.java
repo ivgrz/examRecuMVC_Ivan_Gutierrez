879273f794051d289f26f5b7bdeb5d8227b2539b
Ivan <igutierrezcontreras@danielcastelao.org>
2025-06-18 13:55:25 +0200

    Comentario_app_entrada

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
+    /**
+     * Funcion main que se muestra por pantalla
+     * @param args
+     */
     public static void main(String[] args) {
+
+       // Entrada a la View para que el usuario pueda usar el menu
         View.menu();
     }
 }
