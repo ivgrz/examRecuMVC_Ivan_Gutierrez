475f641f2a2c502ef4b0ae388c9a7facb082f7ff
Ivan <igutierrezcontreras@danielcastelao.org>
2025-06-18 13:51:02 +0200

    Datos_corregido_MVC

--- src/Datos.java
+++ src/Datos.java
@@ -4,7 +4,31 @@ public class Datos {
     private int resultado;
 
 
-    public Datos(int numero1, int numero2,int resultado) {
+    public int getNumero1() {
+        return numero1;
+    }
+
+    public void setNumero1(int numero1) {
+        this.numero1 = numero1;
+    }
+
+    public int getNumero2() {
+        return numero2;
+    }
+
+    public void setNumero2(int numero2) {
+        this.numero2 = numero2;
+    }
+
+    public int getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(int resultado) {
+        this.resultado = resultado;
+    }
+
+    public Datos(int numero1, int numero2, int resultado) {
         this.numero1 = numero1;
         this.numero2 = numero2;
         this.resultado = resultado;
