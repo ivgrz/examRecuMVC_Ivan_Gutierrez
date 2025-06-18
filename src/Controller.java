6d6cb21b0df630844bfb0b04c27b7be5cd918894
Ivan <igutierrezcontreras@danielcastelao.org>
2025-06-18 13:50:49 +0200

    Controller_terminado_MVC

--- src/Controller.java
+++ src/Controller.java
@@ -1,2 +1,24 @@
 public class Controller {
+
+    public static Datos obtenerDatosC(int numero1, int numero2, int resultado) {
+        Datos datos = new Datos(numero1, numero2, resultado);
+        Model model = new Model();
+        return datos = model.obtenerDatos(numero1, numero2, resultado);
+
+
+    }
+
+    public static int SumarC(int numero1, int numero2, int resultado){
+        return Model.SumarM(numero1, numero2, resultado);
+    }
+    public static  int RestarC(int numero1, int numero2, int resultado){
+        return Model.RestarM(numero1, numero2, resultado);
+    }
+    public static int MultiplicarC(int numero1, int numero2, int resultado){
+        return Model.MultiplicarM(numero1, numero2, resultado);
+    }
+    public static boolean DivisionC(int numero1, int numero2, int resultado){
+        return Model.DivisionM(numero1, numero2, resultado);
+    }
+
 }
